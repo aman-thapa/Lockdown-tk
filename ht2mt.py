2c3554cfc18a608cc9155d95a5b1bd06dfa0c52a
Aman <amythapa3032@gmail.com>
2020-10-09 21:07:56 +0530

    initial interface

---
+++ ht2mt.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, DoubleVar, Entry
+
+window = Tk()
+window.title("Feet to Meter Conversion App")
+window.configure(background="navy blue")
+window.geometry("320x220")
+window.resizable(width=False, height=False)
+
+window.mainloop()
