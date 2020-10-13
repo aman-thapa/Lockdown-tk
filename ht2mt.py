17ecd446aa58b5ea368808c2281f389d743facfc
Aman <amythapa3032@gmail.com>
2020-10-13 22:31:21 +0530

    Updated Version

--- ht2mt.py
+++ ht2mt.py
@@ -26,15 +26,18 @@ ft_entry = Entry(window, textvariable=ft_value, width=14)
 ft_entry.grid(column=1, row=0)
 ft_entry.delete(0, 'end')
 
+ft_lbl = Label(window, text="Feet", bg="purple", fg="white", width=14)
+ft_lbl.grid(column=0, row=0, padx=15, pady=15)
+
 mt_lbl = Label(window, text="Meter", bg="brown", fg="white", width=14)
 mt_lbl.grid(column=0, row=1)
 
 mt_value = DoubleVar()
-mt_entry = Entry(window, textvariable=ft_value, width=14)
+mt_entry = Entry(window, textvariable=mt_value, width=14)
 mt_entry.grid(column=1, row=1, pady=30)
 mt_entry.delete(0, 'end')
 
-convert_btn = Button(window, text="Convert", bg="orange", fg="white", width=14, command="")
+convert_btn = Button(window, text="Convert", bg="orange", fg="white", width=14, command=convert)
 convert_btn.grid(column=0, row=3, padx=15)
 
 clear_btn = Button(window, text="Clear", bg="black", fg="white", width=14, command=clear)
